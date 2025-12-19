28312fd1ed5282d8e950d2b8a45c96fc8aa8e57b
pawel.kowalczuk <pwl.kowalczuk@gmail.com>
2025-12-19 21:03:15 +0100

    Summary moved forward, something is missing in fetching done tasks but dont know what

--- TaskLedger/TaskLedger/Summary/SummaryView.swift
+++ TaskLedger/TaskLedger/Summary/SummaryView.swift
@@ -7,11 +7,30 @@ struct SummaryView: View {
         List {
             ForEach(viewModel.eventsDict.keys.sorted(by: { $0.name < $1.name }), id: \.self) { eventTask in
                 VStack {
-                    Text(eventTask.name)
+                    HStack {
+                        Image(systemName: eventTask.taskType.imageName)
+                        Text(eventTask.name)
+                    }
+                    let summary = viewModel.eventsDict[eventTask]
+                    HStack {
+                        Text(eventTask.taskType.taskName)
+                        if eventTask.taskType == .counter, let counterSummary = summary?.counterSummary {
+                            Text("\(counterSummary)")
+                        } else if eventTask.taskType == .time, let timerSummary = summary?.amountSummary {
+                            Text("\(timerSummary)")
+                        } else if eventTask.taskType == .cost || eventTask.taskType == .income, let summary = summary?.amountSummary {
+                            Text("\(summary)")
+                        }
+                            
+                        
+                    }
+                        
                     
                 }
             }
         }.refreshable {
+            viewModel.fetchData()
+        }.onAppear {
             viewModel.fetchData()
         }
     }
